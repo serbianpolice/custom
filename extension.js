727958c96be02a6e6cd7c75e72a53b5279d01e26
serbianpolice <46035933+serbianpolice@users.noreply.github.com>
2018-12-21 01:04:23 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -51,7 +51,7 @@
         chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
         scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
         roomLock: false, // Requires an extension to re-load the script
-        startupCap: 10, // 1-200
+        startupCap: 1, // 1-200
         startupVolume: 100, // 0-100
         startupEmoji: true, // true or false
         autowoot: true,
